4a2122e5c266b3b1bc6dc29d6c2247f5659f7a54
charlykop <charly-ko@hotmail.de>
2023-12-05 15:51:59 +0100

    Solution for exercise_5 (preprocessing and clean data)  in run.py the function go was written, conda.yml and MLproject were created.

--- lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
+++ lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
@@ -13,8 +13,39 @@ def go(args):
 
     run = wandb.init(project="exercise_5", job_type="process_data")
 
-    ## YOUR CODE HERE
-    pass
+    logger.info("Getting artifact")
+    artifact = run.use_artifact(args.input_artifact)
+    local_path = artifact.file()
+    logger.info(f"Artifact was downloaded to {local_path}.")
+
+    logger.info("Transform input artifact to pandas dataframe.")
+    df = pd.read_parquet(local_path)
+
+    logger.info("Delete duplicates.")
+    df = df.drop_duplicates().reset_index(drop=True)
+
+    logger.info("Add new feature - combination of features titel and son_name.")
+    df['title'].fillna(value='', inplace=True)
+    df['song_name'].fillna(value='', inplace=True)
+    df['text_feature'] = df['title'] + ' ' + df['song_name']
+
+    logger.info("Tranform output artifact to csv.")
+    filename = args.artifact_name
+    df.to_csv(filename)
+
+    logger.info("Creating artifact.")
+    output_artifact = wandb.Artifact(
+        name=args.artifact_name, 
+        type=args.artifact_type, 
+        description=args.artifact_description
+    ) 
+
+    logger.info("Add artifact and log artifact.")
+    output_artifact.add_file(filename)
+    run.log_artifact(output_artifact)
+    run.finish()
+
+
 
 
 if __name__ == "__main__":
